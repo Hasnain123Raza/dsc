3a564a0111c2bce13a3ea97349a96b45f534437a
Hasnain123Raza <Hasnain123Raza@users.noreply.github.com>
2022-04-03 22:26:10 +0000

    deploy: b0e3edf465bfee1f7aeafbf64381fa621fde8161

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['dsc_5farray_5fqueue_5finitial_5fcapacity_0',['DSC_ARRAY_QUEUE_INITIAL_CAPACITY',['../array__queue_8h.html#acac24270d38ea7aeaebac115634ad488',1,'array_queue.h']]],
-  ['dsc_5farray_5fstack_5finitial_5fcapacity_1',['DSC_ARRAY_STACK_INITIAL_CAPACITY',['../array__stack_8h.html#ae970be379b0d3af1e4fcbafeb4d8dfd4',1,'array_stack.h']]],
-  ['dsc_5fdynamic_5flist_5finitial_5fcapacity_2',['DSC_DYNAMIC_LIST_INITIAL_CAPACITY',['../dynamic__list_8h.html#a72a14fc6d33172eb7d21b5fb761e6dbc',1,'dynamic_list.h']]]
+  ['dsc_5fdynamic_5flist_5finitial_5fcapacity_0',['DSC_DYNAMIC_LIST_INITIAL_CAPACITY',['../dynamic__list_8h.html#a72a14fc6d33172eb7d21b5fb761e6dbc',1,'dynamic_list.h']]]
 ];
